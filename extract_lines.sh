467aecf4d261e5043183532aebb6dc3651f347a0
Noémie <noemie.lehuby@zaclys.net>
2019-04-10 17:26:09 +0200

    init repository

---
+++ extract_lines.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ev
+
+#wget http://download.geofabrik.de/europe/france-latest.osm.pbf --no-verbose --output-document=data.osm.pbf 2>&1
+wget http://download.geofabrik.de/europe/monaco-latest.osm.pbf --no-verbose --output-document=data.osm.pbf 2>&1
+
+osm_transit_extractor -i data.osm.pbf
+
+mkdir output
+
+cat osm-transit-extractor_lines.csv|xsv select 1-8 |xsv search -s shape '^$' -v > output/lines_from_osm.csv
