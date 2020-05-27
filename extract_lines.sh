cf665ea10400586c476ca29f860cd5722b51bef0
Noémie <noemie.lehuby@zaclys.net>
2020-05-27 18:48:18 +0200

    sans les tracés

--- extract_lines.sh
+++ extract_lines.sh
@@ -9,10 +9,10 @@ osm_transit_extractor -i data.osm.pbf
 
 mkdir output
 
-cat osm-transit-extractor_lines.csv | xsv search -s shape '^$' -v > lines_with_shapes.csv
+#cat osm-transit-extractor_lines.csv | xsv search -s shape '^$' -v > lines_with_shapes.csv
 
 #ogr2ogr output/lines.geojson -dialect sqlite -sql "SELECT *, GeomFromText(shape) FROM lines_with_shapes" lines_with_shapes.csv -a_srs "WGS84"
-ogr2ogr -f "ESRI Shapefile" output/lines_with_shape.shp -dialect sqlite -sql "SELECT *, GeomFromText(shape) FROM lines_with_shapes" lines_with_shapes.csv -a_srs "WGS84" -lco ENCODING=UTF-8
+#ogr2ogr -f "ESRI Shapefile" output/lines_with_shape.shp -dialect sqlite -sql "SELECT *, GeomFromText(shape) FROM lines_with_shapes" lines_with_shapes.csv -a_srs "WGS84" -lco ENCODING=UTF-8
 
 
-cat lines_with_shapes.csv |xsv select '!shape' > output/lines.csv
+cat osm-transit-extractor_lines.csv |xsv select '!shape' > output/lines.csv
