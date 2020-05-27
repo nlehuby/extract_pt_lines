6b36fc367a7a1c6f4891901c97873ae40f89b117
Noémie <noemie.lehuby@zaclys.net>
2020-05-27 16:01:27 +0200

    extract shp & csv

--- extract_lines.sh
+++ extract_lines.sh
@@ -2,11 +2,17 @@
 
 set -ev
 
-wget http://download.geofabrik.de/europe/france-latest.osm.pbf --no-verbose --output-document=data.osm.pbf 2>&1
-#wget http://download.geofabrik.de/europe/monaco-latest.osm.pbf --no-verbose --output-document=data.osm.pbf 2>&1
+#wget http://download.geofabrik.de/europe/france-latest.osm.pbf --no-verbose --output-document=data.osm.pbf 2>&1
+wget http://download.geofabrik.de/europe/monaco-latest.osm.pbf --no-verbose --output-document=data.osm.pbf 2>&1
 
 osm_transit_extractor -i data.osm.pbf
 
 mkdir output
 
-cat osm-transit-extractor_lines.csv|xsv select 1-8 |xsv search -s shape '^$' -v > output/lines_from_osm.csv
+cat osm-transit-extractor_lines.csv | xsv search -s shape '^$' -v > lines_with_shapes.csv
+
+#ogr2ogr output/lines.geojson -dialect sqlite -sql "SELECT *, GeomFromText(shape) FROM lines_with_shapes" lines_with_shapes.csv -a_srs "WGS84"
+ogr2ogr -f "ESRI Shapefile" output/lines_with_shape.shp -dialect sqlite -sql "SELECT *, GeomFromText(shape) FROM lines_with_shapes" lines_with_shapes.csv -a_srs "WGS84" -lco ENCODING=UTF-8
+
+
+cat lines_with_shapes.csv |xsv select '!shape' > output/lines.csv
